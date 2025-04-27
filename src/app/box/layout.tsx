dcbbf633db95c04cf401d70f40ba15f0427b7080
yaskou <anmitutarow@gmail.com>
2025-04-27 20:20:27 +0900

    fix: ロゴをクリックしてホームに戻れるように修正

--- src/app/box/layout.tsx
+++ src/app/box/layout.tsx
@@ -8,12 +8,13 @@ export default function BoxLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="w-full h-svh flex flex-col">
       <header className="w-full p-2 md:p-4 flex gap-2 md:gap-4">
-        <div className="text-3xl grow flex gap-1 font-extrabold tracking-tight md:text-4xl">
+        <Link
+          className="text-3xl grow flex gap-1 font-extrabold tracking-tight md:text-4xl"
+          href="/box"
+        >
           <Boxes className="h-full" />
-          <Link className="hidden sm:block" href="/box">
-            BlueBox
-          </Link>
-        </div>
+          <h1 className="hidden sm:block">BlueBox</h1>
+        </Link>
         <UploaderDialog />
         <form
           action={async () => {
